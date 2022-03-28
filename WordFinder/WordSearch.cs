ff06bec6bbcb7df15afc839e2f7c670020a89d9a
Carlos Ruiz <carlos.ruiz@projekt202.com>
2022-03-28 00:51:30 -0500

    Add project files.

---
+++ WordFinder/WordSearch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordFinder
+{
+    class WordSearch
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
